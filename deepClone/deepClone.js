264dc7c12b4452dc39c187ededf973bf4e7fe903
Hoya-kim <1203play@gmail.com>
2021-09-23 14:51:52 +0900

    refactor: modify deepClone.js
    decrease depth, use 'const' not 'let'

--- deepClone/deepClone.js
+++ deepClone/deepClone.js
@@ -1,15 +1,12 @@
 function deepClone(target) {
-  let clone = null;
-  if (typeof target === 'object' && target !== null) {
-    // Array or Object
-    clone = target.constructor === Array ? [] : {};
-    Object.keys(target).forEach(key => {
-      clone[key] = deepClone(target[key]);
-    });
-  } else {
-    // not object(primitive, function, null, ...)
-    clone = target;
-  }
+  // Not object(primitive, function, null, ...)
+  if (typeof target !== 'object' || target === null) return target;
+
+  // Array or Object
+  const clone = target.constructor === Array ? [] : {};
+  Object.keys(target).forEach(key => {
+    clone[key] = deepClone(target[key]);
+  });
   return clone;
 }
 
