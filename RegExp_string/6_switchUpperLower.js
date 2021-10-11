ef6f87683425fc53c3c2ff171082d4d995a8f853
Hoya-kim <1203play@gmail.com>
2021-10-11 11:33:55 +0900

    feat: update regex practice6

--- RegExp_string/6_switchUpperLower.js
+++ RegExp_string/6_switchUpperLower.js
@@ -1,4 +1,9 @@
+// const toggleCase = str =>
+//   str.replace(/./g, s => (s === s.toUpperCase() ? s.toLowerCase() : s.toUpperCase()));
+
 const toggleCase = str =>
-  str.replace(/./g, s => (s === s.toUpperCase() ? s.toLowerCase() : s.toUpperCase()));
+  str.replace(/([a-z]+)|([A-Z]+)/g, (_, lowerCase, upperCase) =>
+    lowerCase ? lowerCase.toUpperCase() : upperCase.toLowerCase(),
+  );
 
 console.log(toggleCase('StuDY')); // => 'sTUdy'
