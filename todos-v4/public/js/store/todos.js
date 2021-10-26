49d0d981232eefcc29db99e4721c0775ba28d6f0
Hoya-kim <1203play@gmail.com>
2021-10-26 13:48:58 +0900

    feat: Add modularize todos for add mock api

---
+++ todos-v4/public/js/store/todos.js
@@ -0,0 +1,85 @@
+const store = {
+  state: {
+    todos: [],
+    currentFilter: 'all',
+  },
+
+  // observer
+  listeners: [],
+  notify() {
+    console.log('[STATE]', this.state);
+    this.listeners.forEach(listener => listener(this.state));
+  },
+
+  get todos() {
+    return this.state.todos;
+  },
+  set todos(newTodos) {
+    this.state.todos = newTodos;
+    this.notify();
+  },
+  get currentFilter() {
+    return this.state.currentFilter;
+  },
+  set currentFilter(newFilter) {
+    this.state.currentFilter = newFilter;
+    this.notify();
+  },
+};
+
+// observer pattern
+const subscribe = listener => {
+  store.listeners.push(listener);
+};
+
+const fetchTodos = () => {
+  store.todos = [
+    { id: 3, content: 'JavaScript', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 1, content: 'HTML', completed: false },
+  ];
+};
+
+const toggleAllTodos = completed => {
+  store.todos = store.todos.map(todo => ({ ...todo, completed }));
+};
+
+const generateId = () => Math.max(...store.todos.map(todo => todo.id), 0) + 1;
+
+const addTodo = content => {
+  store.todos = [{ id: generateId(), content, completed: false }, ...store.todos];
+};
+
+const toggleTodo = id => {
+  store.todos = store.todos.map(todo =>
+    todo.id === +id ? { ...todo, completed: !todo.completed } : todo,
+  );
+};
+
+const updateTodoContent = (id, content) => {
+  store.todos = store.todos.map(todo => (todo.id === +id ? { ...todo, content } : todo));
+};
+
+const removeTodo = id => {
+  store.todos = store.todos.filter(todo => todo.id !== +id);
+};
+
+const removeAllCompletedTodos = () => {
+  store.todos = store.todos.filter(todo => !todo.completed);
+};
+
+const setFilter = filter => {
+  store.currentFilter = filter;
+};
+
+export default {
+  subscribe,
+  fetchTodos,
+  toggleAllTodos,
+  addTodo,
+  toggleTodo,
+  updateTodoContent,
+  removeTodo,
+  removeAllCompletedTodos,
+  setFilter,
+};
